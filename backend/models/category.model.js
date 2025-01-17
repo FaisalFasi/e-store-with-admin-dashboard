07636f151af77c1befef5658f85e5226bf386a30
faisalrehman <faisalfasi18@gmail.com>
2025-01-17 22:08:44 +0100

    feat: simplify category model; remove unused subcategory structure and clean up default categories helper

--- backend/models/category.model.js
+++ backend/models/category.model.js
@@ -28,30 +28,21 @@ const categorySchema = new mongoose.Schema(
       ref: "Category",
       default: null, // for top-level categories
     },
-    subCategories: [
-      // {
-      //   type: mongoose.Schema.Types.ObjectId,
-      //   ref: "Category",
-      // },
-    ],
+    subCategories: [],
     status: {
       type: String,
       enum: ["active", "inactive"],
       default: "active",
     },
-    // When to use: This field is used to sort categories in the frontend. The lower the number, the higher the category will appear in the list.
     sortOrder: {
       type: Number,
       default: 0,
     },
-    // When to use: Only provide this field if you want to set a custom SEO title for the category. If it's not provided, it defaults to an empty string (""), meaning no special title is set.
 
     metaTitle: {
       type: String,
       default: "",
     },
-
-    // When to use: Similar to metaTitle, you only need to provide this field if you want to set a custom description for search engines. If not provided, it defaults to an empty string ("").
 
     metaDescription: {
       type: String,
