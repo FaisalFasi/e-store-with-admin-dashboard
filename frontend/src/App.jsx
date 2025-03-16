f0d588078a5fde655ea1315dbd5666b50d2024ce
faisalrehman <faisalfasi18@gmail.com>
2025-03-16 12:40:04 +0100

    removed botpress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import _app from "./components/_app/_app";
-import BotPress from "./components/shared/chatbot/BotPress";
 import "./App.css";
 
 function App() {
   return (
     <>
       <_app />
-      {/* <BotPress /> */}
     </>
   );
 }
