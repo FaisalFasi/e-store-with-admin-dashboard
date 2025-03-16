e3384bff0384fd5650ee3a47b7f7fd786170ac9b
faisalrehman <faisalfasi18@gmail.com>
2025-03-16 12:32:17 +0100

    rollup plugin visualizer added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true, // Automatically open the report in the browser
+      filename: "bundle-analysis.html", // Output file
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the warning limit if needed
+  },
 
   resolve: {
     alias: {
